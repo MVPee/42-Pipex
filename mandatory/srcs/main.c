3f0b2caaaf41e4b2514b859a89bd33d1a0a58d70
MVPee <mariusvanpee@hotmail.be>
2023-12-12 00:35:18 +0100

    add parameters and ft exit

--- mandatory/srcs/main.c
+++ mandatory/srcs/main.c
@@ -6,44 +6,30 @@
 /*   By: mvpee <mvpee@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 16:40:38 by mvpee             #+#    #+#             */
-/*   Updated: 2023/12/11 21:04:24 by mvpee            ###   ########.fr       */
+/*   Updated: 2023/12/12 00:33:35 by mvpee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
 
-static void parent_process(t_data data, int *fd, char **env)
+static void parent_process(t_data *data, int *fd, char **env)
 {
 	close(fd[1]);
 	dup2(fd[0], STDIN_FILENO);
 	close(fd[0]);
-	dup2(data.output, STDOUT_FILENO);
-	if (execve(data.cmd2, (char *[]){data.cmd2, NULL}, env) == -1)
-	{
-		close(data.output);
-		ft_free(2, &data.cmd, &data.cmd2);
-		perror("execve parent");
-		exit(EXIT_FAILURE);
-	}
-	close(data.output);
-	ft_free(1, &data.cmd2);
+	dup2(data->output, STDOUT_FILENO);
+	if (execve(data->cmd2, data->s_cmd2, env) == -1)
+		ft_exit(data, "execve parent", EXIT_FAILURE);
 }
 
-static void child_process(t_data data, int *fd, char **env)
+static void child_process(t_data *data, int *fd, char **env)
 {
 	close(fd[0]);
     dup2(fd[1], STDOUT_FILENO);
-	dup2(data.input, STDIN_FILENO);
+	dup2(data->input, STDIN_FILENO);
     close(fd[1]);
-    if (execve(data.cmd, (char *[]){data.cmd, NULL}, env) == -1)
-	{
-		close(data.input);
-		ft_free(2, &data.cmd, &data.cmd2);
-		perror("execve child");
-		exit(EXIT_FAILURE);
-	}
-	close(data.input);
-	ft_free(1, &data.cmd);
+    if (execve(data->cmd, data->s_cmd, env) == -1)
+		ft_exit(data, "execve child", EXIT_FAILURE);
 }
 
 int	main(int ac, char **av, char **env)
@@ -55,32 +41,21 @@ int	main(int ac, char **av, char **env)
 	// Arg check
 	if (ac != 5)
 		return (ft_printf_fd(2, RED "E-01:\nNumber(s) of args is invalid...\n" RESET), 1);
-	data.input = open(av[1], O_RDONLY);
-	if (data.input == -1)
-		return (ft_printf_fd(2, RED "E-02:\nInput doesn't exist...\n" RESET), 1);
-	data.output = open(av[4], O_CREAT | O_TRUNC | O_WRONLY, 0644);
-	data.cmd = find_path(env, av[2]);
-	if (!data.cmd)
-		return (ft_printf_fd(2, RED "E-03:\nCMD fail...\n" RESET), 1);
-    data.cmd2 = find_path(env, av[3]);
-	if (!data.cmd2)
-		return (ft_free(1, &data.cmd), ft_printf_fd(2, RED "E-03.1:\nCMD2 fail...\n" RESET), 1);
+	if (arg_check(&data, av, env))
+		return (2);
 
 	// Unit Test
     ft_printf(GREEN "\nCMD: %s\nPIPE: %s\n\n" RESET, data.cmd, data.cmd2);
 	
 	// Process
     if (pipe(fd) == -1)
-		return (ft_free(2, &data.cmd, &data.cmd2), ft_printf_fd(2, RED "E-03.1:\nPIPE fail...\n" RESET), 1);
+		return (ft_free(2, &data.cmd, &data.cmd2), ft_printf_fd(2, RED "E-03.1:\nPIPE fail...\n" RESET), 3);
     pid = fork();
 	if (pid == -1)
-		return (ft_free(2, &data.cmd, &data.cmd2), ft_printf_fd(2, RED "E-04:\nFork fail...\n" RESET), 1);
+		return (ft_free(2, &data.cmd, &data.cmd2), ft_printf_fd(2, RED "E-04:\nFork fail...\n" RESET), 4);
 	if (pid == 0)
-		child_process(data, fd, env);
-	else
-	{
-		parent_process(data, fd, env);
-		waitpid(pid, NULL, 0);
-	}
+		child_process(&data, fd, env);
+	parent_process(&data, fd, env);
+	ft_exit(&data, GREEN "SUCCESS" RESET, EXIT_SUCCESS);
 	return (0);
 }
--- mandatory/srcs/utils.c
+++ mandatory/srcs/utils.c
@@ -6,11 +6,23 @@
 /*   By: mvpee <mvpee@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 17:39:01 by mvpee             #+#    #+#             */
-/*   Updated: 2023/12/11 18:01:09 by mvpee            ###   ########.fr       */
+/*   Updated: 2023/12/12 00:34:57 by mvpee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
+
+void	ft_exit(t_data *data, char *message, int type)
+{
+	ft_free_split(2, &data->s_cmd, &data->s_cmd2);
+	ft_free(2, &data->cmd, &data->cmd2);
+	if (data->input)
+		close(data->input);
+	if (data->output)
+		close(data->output);
+	ft_printf_fd(2, "%s\n", message);
+	exit(type);
+}
 
 char	*find_path(char **env, char *cmd)
 {
@@ -42,4 +54,25 @@ char	*find_path(char **env, char *cmd)
 		ft_free(2, &path, &temp);
 	}
 	return (NULL);
+}
+
+int arg_check(t_data *data, char **av, char **env)
+{
+	data->input = open(av[1], O_RDONLY);
+	if (data->input == -1)
+		return (ft_printf_fd(2, RED "E-02:\nInput doesn't exist...\n" RESET), 1);
+	data->output = open(av[4], O_CREAT | O_TRUNC | O_WRONLY, 0644);
+	data->s_cmd = ft_split(av[2], " ");
+	if(!data->s_cmd)
+		return (ft_printf_fd(2, RED "E-02.1:\nSplit Fail\n" RESET), 1);
+	data->s_cmd2 = ft_split(av[3], " ");
+	if(!data->s_cmd2)
+		return (ft_free_split(1, &data->s_cmd), ft_printf_fd(2, RED "E-02.2\nSplit2 Fail\n" RESET));
+	data->cmd = find_path(env, data->s_cmd[0]);
+	if (!data->cmd)
+		return (ft_free_split(2, &data->s_cmd, &data->s_cmd2), ft_printf_fd(2, RED "E-03:\nCMD fail...\n" RESET));
+    data->cmd2 = find_path(env, data->s_cmd2[0]);
+	if (!data->cmd2)
+		return (ft_free_split(2, &data->s_cmd, &data->s_cmd2), ft_free(1, &data->cmd), ft_printf_fd(2, RED "E-03.1:\nCMD2 fail...\n" RESET));
+	return (0);
 }
